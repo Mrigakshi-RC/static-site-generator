7ab19e99229076de70574df76333958328166b24
Mrigakshi-RC <mrigakshirc@gmail.com>
2026-03-19 06:23:43 +0000

    add: deployment configs

--- src/helpers/file_utils.py
+++ src/helpers/file_utils.py
@@ -26,7 +26,7 @@ def copy_folder_contents(source_dir, destination_dir):
     except Exception as e:
         print(e)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     md_content, html_content=None, None
@@ -40,23 +40,23 @@ def generate_page(from_path, template_path, dest_path):
     md_to_html=markdown_to_html_node(md_content).to_html()
     page_title=extract_title(md_content)
 
-    final_html_content=html_content.replace('{{ Title }}', page_title).replace('{{ Content }}', md_to_html)
+    final_html_content=html_content.replace('{{ Title }}', page_title).replace('{{ Content }}', md_to_html).replace('href="/',f'href="{basepath}').replace('src="/',f'src="{basepath}')
 
     with open(dest_path, "w") as file:
         file.write(final_html_content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if dir_path_content==None:
         return
     if os.path.isfile(dir_path_content):
         base_name, _ = os.path.splitext(dest_dir_path)
-        generate_page(dir_path_content, template_path, base_name+".html")
+        generate_page(dir_path_content, template_path, base_name+".html", basepath)
     else:
         for item in os.listdir(dir_path_content):
             content_path=os.path.join(dir_path_content, item)
             dest_path=os.path.join(dest_dir_path, item)
             if os.path.isdir(content_path):
                 os.mkdir(dest_path)
-            generate_pages_recursive(content_path, template_path, dest_path)
+            generate_pages_recursive(content_path, template_path, dest_path, basepath)
 
         
--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
+import sys
+
 from helpers.file_utils import copy_folder_contents, delete_folder_contents, generate_page, generate_pages_recursive
 from textnode import TextNode, TextType
 
 def main():
-    delete_folder_contents('public')
-    copy_folder_contents('static', 'public')
-    generate_pages_recursive('content', 'template.html', 'public')
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
+    delete_folder_contents('docs')
+    copy_folder_contents('static', 'docs')
+    generate_pages_recursive("content", 'template.html', 'docs', basepath)
 
 if __name__ == "__main__":
     main()
